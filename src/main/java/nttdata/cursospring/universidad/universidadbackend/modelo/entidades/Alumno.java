30e9ab74a3e131317582ebb1623d86b6abfb0cb2
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-02-02 09:04:52 +0100

    probar git en intellij

--- src/main/java/nttdata/cursospring/universidad/universidadbackend/modelo/entidades/Alumno.java
+++ src/main/java/nttdata/cursospring/universidad/universidadbackend/modelo/entidades/Alumno.java
@@ -10,7 +10,7 @@ public class Alumno extends Persona {
 
 	public Alumno(Integer id, String nombre, String apellido, String dni, Direccion direccion) {
 		super(id, nombre, apellido, dni, direccion);
-		
+		// probar git
 	}
 	
 }
