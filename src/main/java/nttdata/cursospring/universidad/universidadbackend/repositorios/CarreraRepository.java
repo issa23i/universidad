90a2d1d94ede51df7bc963584cb55097c54977bf
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-02-06 12:13:01 +0100

    JPQL vs JPA Query Methods

--- src/main/java/nttdata/cursospring/universidad/universidadbackend/repositorios/CarreraRepository.java
+++ src/main/java/nttdata/cursospring/universidad/universidadbackend/repositorios/CarreraRepository.java
@@ -1,10 +1,15 @@
 package nttdata.cursospring.universidad.universidadbackend.repositorios;
 
 import nttdata.cursospring.universidad.universidadbackend.modelo.entidades.Carrera;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
 public interface CarreraRepository extends CrudRepository<Carrera, Integer> {
-
-
+    //@Query("select c from Carrera c where c.nombre like %?1")
+    Iterable<Carrera> findCarrerasByNombreContains(String nombreCarrera);
+    //@Query("select c from Carrera c where c.nombre like upper(%?1)")
+    Iterable<Carrera> findCarrerasByNombreContainsIgnoreCase(String nombreCarrera);
+    //@Query("select c from Carrera c where c.cantidadAnyos > ?1")
+    Iterable<Carrera> findCarrerasByCantidadAniosAfter(Integer cantidadAnyos);
 }
